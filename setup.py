6bb5aa6db2975207d2fffb8fcb779bff9b17d3fa
Flesh-vadim22 <vadyusha.shubin.2001@mail.ru>
2022-07-14 18:23:39 +0300

    add new models

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='detection drones',
+    author='Shubin Vadim',
+    license='MIT',
+)
